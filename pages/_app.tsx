c21c09187962a847b2b5d65b134700da7b27acc8
KittimasDev <kittimasdev@gmail.com>
2024-11-14 09:40:42 +0700

    [ update ]

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { Session } from "next-auth";
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ session?: Session }>) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
